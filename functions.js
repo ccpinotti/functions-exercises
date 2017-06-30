309ca822ce78efc4c3bec610137992ef3119d89b
ccpinotti <ccpinotti@gmail.com>
2017-06-29 17:52:50 -0700

    have code for 3 of 12 but not one is not operating, will continue over weekend

--- functions.js
+++ functions.js
@@ -1,1 +1,24 @@
 // TODO: add your solutions here!
+1. Build your own concatenation
+function combineWords(word1, word2) {
+  let answer = word1 + word2;
+  return answer
+}
+
+2. Repeat a phrase
+function repeatWords(phrase, n){
+
+  for (let i = 0; i < n; i++){
+  console.log(phrase);
+}};
+
+3. Build your own Power function
+
+//not working
+function powerTo(a, b){
+  Math.pow();
+}
+var result = powerTo(4, 5);
+console.log(result);
+
+4. Area of cirlce : 3.14 r squared
